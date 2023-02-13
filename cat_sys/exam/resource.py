f109bd6f9e90252fbdef058db3d0a926215e47f7
lxy <pku_spcs_lxy@pku.edu.cn>
2023-02-13 22:19:53 +0800

    #

--- cat_sys/exam/resource.py
+++ cat_sys/exam/resource.py
@@ -1,5 +1,7 @@
 from import_export import resources
 from .models import Student,Test,Item,Record
+from collections import OrderedDict
+import json
 
 class StudentResource(resources.ModelResource):
     class Meta:
@@ -11,11 +13,27 @@ class StudentResource(resources.ModelResource):
 class ItemResource(resources.ModelResource):
     def before_import_row(self, row, **kwargs):
         try:
-            print(row)
-            row['options'] = "{'a':'a'}"
+            title = row['title']
+            types = row['types']
+            
+            options_row = OrderedDict()
+            try:
+                for i in range(100):
+                    options_row[f'opt_{i+1}'] = row[f'opt_{i+1}']
+            except Exception as e:
+                pass
+            options = json.dumps(options_row,ensure_ascii=False)
+
+
+
+            row.clear()
+            row['title'] = title
+            row['types'] = types
+            row['options'] = options
             row['answer'] = "{'a':'a'}"
             row['parameters'] = "{'a':'a'}"
             row['dimensions'] = "{'a':'a'}"
+            
         except Exception as e:
             print(e)
 
@@ -32,4 +50,5 @@ class ItemResource(resources.ModelResource):
             'parameters',
             'dimensions'
             ]
-        exclude = ['id']
+        exclude = ['id',]
+        import_id_fields = ('title',)
--- cat_sys/exam/views.py
+++ cat_sys/exam/views.py
@@ -1,3 +1,4 @@
+# coding=utf8
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.urls import reverse
